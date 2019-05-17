5e6a2d37939cf601b91d68384e6afd85699695a8
yangxiaoxiao <yangxiaoxiao@iqianjin.com>
2019-05-17 18:41:26 +0800

    增加作业5的类文件

--- src/main/java/com/imooc/work5/PlayList.java
+++ src/main/java/com/imooc/work5/PlayList.java
@@ -1,39 +1,71 @@
 package com.imooc.work5;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 public class PlayList {
     private String playListName;
     List <Song> musicList=new ArrayList<Song>();
+    public String getPlayListName() {
+        return playListName;
+    }
+
+    public void setPlayListName(String playListName) {
+        this.playListName = playListName;
+    }
+
+    public List<Song> getMusicList() {
+        return musicList;
+    }
+
+    public void setMusicList(List<Song> musicList) {
+        this.musicList = musicList;
+    }
+
     //添加歌曲到播放器列表
     public void addToPlayList(Song song){
-
+        musicList.add(song);
     }
 
     //显示播放列表中所有歌曲：
     public void displayAllSong(){
-
+        for (int i = 0; i < musicList.size(); i++) {
+            System.out.println(musicList.get(i));
+        }
 
     }
 
     //    	－通过id查询歌曲：
     public Song searchSongById(String id){
-
+        Song song=null;
+        for (int i = 0; i <musicList.size() ; i++) {
+            if(musicList.get(i).getId()==id)
+                song=musicList.get(i);
+        }
+        return song;
     }
     //    	－通过名称查询歌曲：
     public Song searchSongByName(String n){
+        Song song=null;
+        for (int i = 0; i < musicList.size(); i++) {
+            if(musicList.get(i).getName().equals(n))
+                song=musicList.get(i);
+        }
+        return song;
     }
 
     //    	－修改歌曲：
     public void updateSong(String id,Song song){
-
+        Song s=searchSongById(id);
+        musicList.remove(s);
+        s=song;
+        musicList.add(song);
     }
 
     //    	－从播放列表删除歌曲：
     public void deleteSong(String id){
-
+        Song song=searchSongById(id);
+        musicList.remove(song);
     }
 
     //             - 导出歌单 ：
--- src/main/java/com/imooc/work5/PlayListCollection.java
+++ src/main/java/com/imooc/work5/PlayListCollection.java
@@ -1,4 +1,36 @@
 package com.imooc.work5;
 
+import java.util.Iterator;
+import java.util.Map;
+
 public class PlayListCollection {
-}
+
+    private static Map<String,PlayList >playListMap;
+
+    public static Map<String, PlayList> getPlayListMap() {
+        return playListMap;
+    }
+
+    public static void setPlayListMap(Map<String, PlayList> playListMap) {
+        PlayListCollection.playListMap = playListMap;
+    }
+
+    public void addPlayList(PlayList playList){
+        playListMap.put(playList.getPlayListName(),playList);
+    }
+    public void deletePlayList(PlayList playList){
+        playListMap.remove(playList.getPlayListName());
+    }
+    public PlayList searchPlayListByName(String playListName){
+        return playListMap.get(playListName);
+    }
+    public void displayPlayListName(){
+        Iterator<PlayList> it = playListMap.values().iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+            }
+        }
+
+    }
+
+
